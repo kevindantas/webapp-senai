49d2b445a9be7295f9165190823f684fab43f230
kevindantas <kevin.ds.29@gmail.com>
2016-06-20 21:11:44 -0300

    Register service worker

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,11 @@
  *           COLOQUE SEU CÓDIGO AQUI EM BAIXO               *
  *                                                          *
  ************************************************************/
+if(navigator.serviceWorker) {
+
+  navigator.serviceWorker.register('./sw.js').then(function () {
+    console.log('Service Worker registrado!!!');
+  }).catch(function () {
+    console.log('Service Worker deu pau :(');
+  });
+}
