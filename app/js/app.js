cf9afe47f4492e7a8b9afe3bce35e8fa4847e538
Kevin-ds <kevin.ds.29@gmail.com>
2016-06-26 12:20:54 -0300

    Sidenav

--- app/js/app.js
+++ app/js/app.js
@@ -2,3 +2,13 @@
 setTimeout(_ => {
   new Toast('Cache completo');
 }, 2000);
+
+document.querySelector('.toggle-menu').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  var mainMenu = document.querySelector('.main-menu');
+  var overlay = document.querySelector('.overlay');
+
+  mainMenu.classList.add('-active');
+  overlay.classList.add('-active');
+});
